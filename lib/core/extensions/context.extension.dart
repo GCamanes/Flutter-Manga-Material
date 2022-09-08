dfcae824e3fc8d7c231ace3837ffd6c94b6c2b5e
Guillaume Camanes <guillaume.camanes@gmail.com>
2022-09-08 20:51:18 +0200

    Revert "Feature/localization" (#2)

--- lib/core/extensions/context.extension.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-extension ContextExtension on BuildContext {
-  Locale get currentLocale => Localizations.localeOf(this);
-
-  AppLocalizations get trad => AppLocalizations.of(this)!;
-}
