0e92ba43c3329fb12ded16414baf5abe9a012e36
GCamanes <guillaume.camanes@gmail.com>
2022-09-22 12:18:26 +0200

    added version number to splashscreen

--- lib/features/splashscreen/presentation/pages/splashscreen.page.dart
+++ lib/features/splashscreen/presentation/pages/splashscreen.page.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:mangamaterial/core/presentation/widget/loader.widget.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SplashscreenPage extends StatelessWidget {
   const SplashscreenPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child:  LoaderWidget(),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            const Expanded(
+              child: Center(child: LoaderWidget()),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: FutureBuilder<PackageInfo>(
+                future: PackageInfo.fromPlatform(),
+                builder: (
+                  BuildContext context,
+                  AsyncSnapshot<PackageInfo> snapshot,
+                ) {
+                  if (snapshot.hasData) {
+                    return Text(
+                      snapshot.data?.version ?? '',
+                      style: Theme.of(context).textTheme.subtitle2?.copyWith(
+                            color: Theme.of(context).colorScheme.onSurface,
+                          ),
+                    );
+                  }
+                  return const SizedBox();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
