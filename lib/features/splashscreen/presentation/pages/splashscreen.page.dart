702de27ec399897cdd22fb4972705dc98d958252
GCamanes <guillaume.camanes@gmail.com>
2022-09-22 11:52:10 +0200

    added text theme and custom loader widget

--- lib/features/splashscreen/presentation/pages/splashscreen.page.dart
+++ lib/features/splashscreen/presentation/pages/splashscreen.page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mangamaterial/core/presentation/widget/loader.widget.dart';
 
 class SplashscreenPage extends StatelessWidget {
   const SplashscreenPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Center(
+        child:  LoaderWidget(),
+      ),
+    );
   }
 }
