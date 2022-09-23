33a1be47d2e16900b9d3aee46d51165b57e8e4d3
GCamanes <guillaume.camanes@gmail.com>
2022-09-23 16:29:57 +0200

    fixed clipping effect on navigation

--- lib/core/navigation/main.page.dart
+++ lib/core/navigation/main.page.dart
@@ -8,6 +8,7 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AutoTabsScaffold(
+      animationDuration: Duration.zero,
       routes: const [
         HomeRoute(),
         FavoritesRoute(),
