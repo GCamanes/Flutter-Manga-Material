a1cc9d3abec36dfbec628b41c6963ff17e77c2f2
GCamanes <guillaume.camanes@gmail.com>
2022-09-28 22:04:32 +0200

    added session restoring on splashscreen

--- lib/features/splashscreen/presentation/pages/splashscreen.page.dart
+++ lib/features/splashscreen/presentation/pages/splashscreen.page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mangamaterial/core/navigation/app.router.dart';
 import 'package:mangamaterial/core/navigation/app.router.gr.dart';
+import 'package:mangamaterial/core/presentation/cubit/custom.cubit.state.dart';
 import 'package:mangamaterial/core/presentation/widget/loader.widget.dart';
+import 'package:mangamaterial/features/login/presentation/cubit/login.cubit.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 class SplashscreenPage extends StatefulWidget {
@@ -14,7 +17,7 @@ class SplashscreenPage extends StatefulWidget {
 class _SplashscreenPageState extends State<SplashscreenPage> {
   @override
   void initState() {
-    _goToMainPage();
+    BlocProvider.of<LoginCubit>(context).getCurrentUser();
     super.initState();
   }
 
@@ -28,33 +31,38 @@ class _SplashscreenPageState extends State<SplashscreenPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            const Expanded(
-              child: Center(child: LoaderWidget()),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: FutureBuilder<PackageInfo>(
-                future: PackageInfo.fromPlatform(),
-                builder: (
-                  BuildContext context,
-                  AsyncSnapshot<PackageInfo> snapshot,
-                ) {
-                  if (snapshot.hasData) {
-                    return Text(
-                      snapshot.data?.version ?? '',
-                      style: Theme.of(context).textTheme.subtitle2?.copyWith(
-                            color: Theme.of(context).colorScheme.onSurface,
-                          ),
-                    );
-                  }
-                  return const SizedBox();
-                },
+      body: BlocListener<LoginCubit, CubitState>(
+        listener: (BuildContext context, CubitState state) {
+          _goToMainPage();
+        },
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              const Expanded(
+                child: Center(child: LoaderWidget()),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: FutureBuilder<PackageInfo>(
+                  future: PackageInfo.fromPlatform(),
+                  builder: (
+                    BuildContext context,
+                    AsyncSnapshot<PackageInfo> snapshot,
+                  ) {
+                    if (snapshot.hasData) {
+                      return Text(
+                        snapshot.data?.version ?? '',
+                        style: Theme.of(context).textTheme.subtitle2?.copyWith(
+                              color: Theme.of(context).colorScheme.onSurface,
+                            ),
+                      );
+                    }
+                    return const SizedBox();
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
