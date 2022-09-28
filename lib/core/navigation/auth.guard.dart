9aa36a8a97c7376aa9f06342ec82bb120cd4f54c
GCamanes <guillaume.camanes@gmail.com>
2022-09-28 22:05:03 +0200

    removed unused import

--- lib/core/navigation/auth.guard.dart
+++ lib/core/navigation/auth.guard.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:mangamaterial/core/navigation/app.router.gr.dart';
 import 'package:mangamaterial/core/presentation/cubit/custom.cubit.state.dart';
